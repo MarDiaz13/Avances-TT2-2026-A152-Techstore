4480ee059a4e20ac0573dc7693eed14c692cac91
MarDiaz13 <163463998+MarDiaz13@users.noreply.github.com>
2026-03-01 20:20:21 -0600

    Delete components directory

--- components/informacionQR.jsx
+++
@@ -1,7 +0,0 @@
-export const informacionQR = {
-    banco: "BBVA Bancomer",
-    titular: "TechStore SA de CV",
-    tarjeta: "**** **** **** 5678",
-    cuenta: "0123 4567 89",
-    clabe: "012345678901234567"
-};
